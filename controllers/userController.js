df96656453717d13842d67cc69a3f6fbd5e3d3bc
ajivenug <ajithvenugopal1996@gmail.com>
2024-10-04 12:58:29 +0530

    register and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,54 @@
 
-const asyncHandler= require("express-async-handler");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt")
+const jwt=require("jsonwebtoken")
 
-const registerUser= asyncHandler(async(req,res)=>{
-
-
-
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const availableUser = await User.findOne({ email });
+    if (availableUser) {
+        res.status(400);
+        throw new Error("User with emailId already exists!");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, password: hashedPassword })
+    if (user) {
+        res.status(201).json({ userId: user.id, username: user.username, email: user.email });
+    } else {
+        res.status(400);
+        throw new Error("User data not valid!!")
+    }
 });
 
-module.exports= {registerUser}
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
+            },
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "30m" }
+        );
+        res.status(200).json({ token: accessToken, userId: user.id, username: user.username });
+    } else {
+        res.status(400);
+        throw new Error("Email or Password not valid!");
+    }
+})
+
+module.exports = { registerUser ,loginUser}
